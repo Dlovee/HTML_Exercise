98d625fadad5e379b9ee9ed43b161c466587ca41
Dlove <dlove327@gmail.com>
2025-11-19 14:51:17 -0600

    Added the .js file

---
+++ index.js
@@ -0,0 +1,14 @@
+// Initialization: Declaration and Assignment
+let productName = "Mentos";
+
+let isSugarFree = true;
+
+let totalPieces = 50;
+
+let sku = "0-73390-01404-9";
+
+let manufacturer; // undefined
+
+// Print values to the browser console with console.log
+
+console.log(totalPieces); // prints 50
