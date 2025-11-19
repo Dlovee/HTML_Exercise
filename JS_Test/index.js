5842d659395aeb05ed7b5dde6c2fc3b83142caef
Dlove <dlove327@gmail.com>
2025-11-19 15:11:33 -0600

    Track index.js file

---
+++ JS_Test/index.js
@@ -0,0 +1,11 @@
+let productName = "Mentos";
+let isSugarFree = true;
+let totalPieces = 50;
+let sku = "0-73390-01404-9";
+let manufacturer;
+
+console.log("Product:", productName);
+console.log("Sugar Free:", isSugarFree);
+console.log("Total Pieces:", totalPieces);
+console.log("SKU:", sku);
+console.log("Manufacturer:", manufacturer);
